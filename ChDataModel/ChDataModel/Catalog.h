bf87c236da99239f75bc15ca879475512a1f6092
Nicolas Morisset <Nicolas.Morisset@unige.ch>
2014-02-04 14:45:32 +0000

    Catalog Class as the class diagram now. TODO: boost test not updated yet
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Alexandria/branches/Alexandria-2.0@3939 463500fe-d6be-4184-9b20-47717e75ecac

--- ChDataModel/ChDataModel/Catalog.h
+++ ChDataModel/ChDataModel/Catalog.h
@@ -1,54 +1,102 @@
 /**
  * @file Catalog.h
  *
- * @author Pierre Dubath
+ * @author Nicolas Morisset
  *
- * Created on: Feb 1, 2013
+ * Created on: Feb 4, 2014
  */
 
 #ifndef CATALOG_H_
 #define CATALOG_H_
 
 #include <map>
+#include <memory>
 #include "ChDataModel/Source.h"
 
 namespace ChDataModel {
 
 /**
- * Catalog contains a container of sources
+ * @class Catalog
+ *
+ * @brief
+ *  Catalog contains a container of sources
+ *
  */
-class Catalog {
+class Catalog
+{
 
 public:
 
-  /// Default constructor
-  Catalog() = default;
+  /**
+   * @brief
+   *  Build a catalog of Source objects
+   *
+   * @details
+   *  Constructs a vector container of Source objects and a map
+   *  of source identification and an index which is the location of the Source
+   *  object in the vector container
+   *
+   * @param
+   *  Vector container of Source objects
+   *
+   * @throw ElementsException
+   *  A Source object can not be inserted twice in the map
+   */
+  Catalog(std::vector<Source> source_vector);
 
-  /// Constructor with member assignment
-  Catalog(std::map<int64_t, Source> source_map) :
-      m_source_map(source_map) {
-  }
+  /**
+   * @brief Destructor
+   */
+  virtual ~Catalog() { }
 
-  /// Virtual default destructor
-  virtual ~Catalog() {
-  }
+  /**
+   * @brief
+   *  Get a const_iterator pointing to the first element in the m_sourceVector
+   *  vector
+   *
+   * @return
+   *  Returns a const_iterator pointing to the first element in the m_sourceVector
+   *  container
+   */
+  std::vector<Source>::const_iterator cbegin() { return m_sourceVector.cbegin() ; }
 
-  const std::map<int64_t, Source>& getSourceMap() const {
-    return m_source_map;
-  }
+  /**
+   * @brief
+   *  Get an const_iterator pointing to the last element in the m_sourceVector
+   *  vector
+   *
+   * @return
+   *  Returns a const_iterator pointing to the past-the-end element in the
+   *  m_sourceVector container
+   */
+  std::vector<Source>::const_iterator cend()   { return m_sourceVector.cend() ; }
 
-  /// Get one source from the container of sources
-  /// @return The source with the given sourceID
-  Source & getSource(int64_t source_id);
+  /**
+   * @brief
+   *  Find the Source object from its identification number
+   *
+   * @param
+   * The source identification number
+   *
+   * @return
+   * A shared pointer to the Source object or a null pointer in case of
+   * no object was found for this source_id
+   */
+  std::shared_ptr<Source> find(const uint64_t source_id);
 
-  /// Add a source into the container of sources in this catalog
-  /// @return Reference to the created source, added to the catalog container
-  void addSource(const Source & source);
+  /**
+   * @brief
+   *  Get the size of the vector container
+   *
+   * @return
+   *  The size of the container which is the number of Source objects
+   */
+  size_t size() { return m_sourceVector.size();}
 
 private:
 
-  /// The source map
-  std::map<int64_t, Source> m_source_map { };
+  std::vector<Source>       m_sourceVector { };
+  std::map<uint64_t, size_t> m_indeces_map { };
 
 };
 
--- ChDataModel/src/Catalog.cpp
+++ ChDataModel/src/Catalog.cpp
@@ -1,13 +1,10 @@
-/*
+/**
  * Catalog.cpp
  *
- *  Created on: Jan 14, 2013
- *      Author: dubath
+ *  Created on : Feb 4, 2014
+ *      Author : Nicolas Morisset
  */
 
-#include <sstream>
-#include <iostream>
-#include <iomanip>
 
 #include "ChDataModel/Catalog.h"
 #include "ChDataModel/Source.h"
@@ -19,44 +16,38 @@ using namespace std;
 namespace ChDataModel {
 
 //-----------------------------------------------------------------------------
-// Add a source to the catalog
-
-void Catalog::addSource(const Source & source) {
-
-  // Insert the source into the container
-  const pair<map<int64_t, Source>::iterator, bool> & p = m_source_map.insert(
-      pair<int64_t, Source>(source.getSourceId(), source));
-
-  // Throw an exception, if that source was already included in the catalog
-  if (p.second == false) {
-    stringstream errorBuffer;
-    errorBuffer << "Catalog::addSource : source " << source.getSourceId()
-        << " is already in this catalog" << endl;
-    throw ElementsException(errorBuffer.str());
+// Constructor
+Catalog::Catalog(vector<Source> source_vector): m_sourceVector(source_vector)
+{
+  // Set the m_indeces_map map
+  for (size_t index=0; index < m_sourceVector.size(); ++index) {
+     auto it = m_indeces_map.emplace(m_sourceVector[index].getId(), index);
+     // Make sure the element does not already exist
+     if (!it.second)
+     {
+       throw ElementsException("Catalog::Catalog: Source object already exist "
+             "in the map for source ID : %d, index: %d\n",
+             m_sourceVector[index].getId(), index);
+     }
   }
+} // Eof Catalog::Catalog
 
-} // Eof Catalog::createSource
 
 //-----------------------------------------------------------------------------
-// Get one source from the container of sources
+// find source in the map
+// return source otherwise null pointer
 
-Source & Catalog::getSource(int64_t source_id) {
-
-  // Try to find it in the container
-  map<int64_t, Source>::iterator it = m_source_map.find(source_id);
-
-  // Throw exception, if that source is not included in the catalog
-  if (it == m_source_map.end()) {
-    stringstream errorBuffer;
-    errorBuffer << "Catalog::getSource : source " << source_id
-        << " is not available in this catalog" << endl;
-    throw ElementsException(errorBuffer.str());
+shared_ptr<Source> Catalog::find(const uint64_t source_id)
+{
+  shared_ptr<Source> ptr(nullptr);
+  auto it = m_indeces_map.find(source_id);
+  if (it != m_indeces_map.end()) {
+    ptr = make_shared<Source>(m_sourceVector[it->second]);
   }
 
-  // Return the source, if found in the container
-  return it->second;
+  return (ptr);
 
-} // Eof Catalog::getSource
+} // Eof Catalog::find
 
 //-----------------------------------------------------------------------------
 
