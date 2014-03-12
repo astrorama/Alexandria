3fe9cee00a643589de9ba8434c5dfbf56f3b3600
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2014-03-12 15:21:57 +0000

    Merge branch 'master' of degauden.isdc.unige.ch:/srv/git/alexandria
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Alexandria/trunk@4162 463500fe-d6be-4184-9b20-47717e75ecac

--- toolchain.cmake
+++
@@ -1,9 +0,0 @@
-# Special toolchain file that inherits the same heptools version as the
-# used projects.
-find_file(inherit_astrotools_module InheritAstroTools.cmake)
-# this check is needed because the toolchain seem to be called a second time
-# without the proper cache
-if(inherit_astrotools_module)
-  include(${inherit_astrotools_module})
-  inherit_astrotools()
-endif()
