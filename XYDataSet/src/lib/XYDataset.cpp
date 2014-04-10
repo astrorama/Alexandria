78e5476e0d19360b5473757569fbd1e126072d49
Nicolas Morisset <Nicolas.Morisset@unige.ch>
2014-04-10 12:05:50 +0000

    Adding the new package XYDataset
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Alexandria/branches/Alexandria-2.0@4486 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ XYDataSet/src/lib/XYDataset.cpp
@@ -0,0 +1,53 @@
+/**
+ * @file XYDataset.cpp
+ *
+ * @date Apr 9, 2014
+ * @author Nicolas Morisset
+ */
+
+#include <utility>
+#include <algorithm>
+#include <iostream>
+
+#include "ElementsKernel/ElementsException.h"
+#include "XYDataset/XYDataset.h"
+
+using namespace std;
+
+namespace XYDataset {
+
+XYDataset::const_iterator XYDataset::begin() const {
+  return m_values.cbegin();
+}
+
+XYDataset::const_iterator XYDataset::end() const {
+  return m_values.cend();
+}
+
+unique_ptr<XYDataset> XYDataset::factory(vector<pair<double, double>> vector_pair)
+{
+  return (unique_ptr<XYDataset> (new XYDataset(vector_pair)));
+}
+
+unique_ptr<XYDataset> XYDataset::factory(std::vector<double> x_vector, std::vector<double> y_vector)
+{
+  size_t x_size = x_vector.size();
+  size_t y_size = y_vector.size();
+  // Vector must have the same size
+  if ( x_size != y_size) {
+    throw ElementsException() << "XYDataset::factory: Vectors must have "
+                              <<"the same size! x size: %d" <<x_size
+                              <<"  y_size : %d"<< y_size;
+  }
+
+  vector<pair<double, double>> vector_pair;
+  vector_pair.reserve(x_size);
+
+  // Make the pair vector
+  transform(x_vector.begin(), x_vector.end(), y_vector.begin(), back_inserter(vector_pair),
+                 [](double a, double b) { return std::make_pair(a, b); });
+
+  return ( unique_ptr<XYDataset> (new XYDataset(vector_pair)) );
+}
+
+} /* namespace XYDataset */
