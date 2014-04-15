dc40f7dc803d5acf675140899ef12551d9fe00b2
Nikolaos Apostolakos <nikoapos@gmail.com>
2014-04-15 20:45:29 +0000

    Added the AsciiReader in the ChTable package.
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Alexandria/branches/Alexandria-2.0@4552 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ ChTable/src/lib/AsciiReader.cpp
@@ -0,0 +1,96 @@
+/** 
+ * @file AsciiReader.cpp
+ * @date April 11, 2014
+ * @author Nikolaos Apostolakos
+ */
+
+#include <utility>
+#include <set>
+// The std regex library is not fully implemented in GCC 4.8. The following lines
+// make use of the BOOST library and can be modified if GCC 4.9 will be used in
+// the future.
+// #include <regex>
+#include <boost/regex.hpp>
+using boost::regex;
+using boost::regex_match;
+#include <boost/algorithm/string.hpp>
+#include "ElementsKernel/ElementsException.h"
+#include "ChTable/AsciiReader.h"
+#include "AsciiReaderHelper.h"
+
+namespace ChTable {
+
+AsciiReader::AsciiReader(std::vector<std::type_index> column_types,
+                         std::vector<std::string> column_names, std::string comment)
+                : m_column_types{std::move(column_types)},
+                  m_column_names{std::move(column_names)},
+                  m_comment{std::move(comment)} {
+  if (m_comment.empty()) {
+    throw ElementsException() << "Empty string as comment indicator";
+  }
+  std::set<std::string> set {};
+  regex whitespace {".*\\s.*"}; // Checks if input contains any whitespace characters
+  for (const auto& name : m_column_names) {
+    if (name.empty()) {
+      throw ElementsException() << "Empty string column names are not allowed";
+    }
+    if (regex_match(name, whitespace)) {
+      throw ElementsException() << "Column name '" << name << "' contains "
+                                << "whitespace characters";
+    }
+    if (!set.insert(name).second) {  // Check for duplicate names
+      throw ElementsException() << "Duplicate column name " << name;
+    }
+  }
+  if (!m_column_names.empty() && !m_column_types.empty()
+      && m_column_names.size() != m_column_types.size()) {
+    throw ElementsException() << "Different number of column names and types";
+  }
+}
+
+const Table AsciiReader::read(std::istream& in) const {
+  size_t columns_number = countColumns(in, m_comment);
+  std::vector<std::string> names {};
+  if (m_column_names.empty()) {
+    names = autoDetectColumnNames(in, m_comment, columns_number);
+  } else {
+    names = m_column_names;
+  }
+  std::vector<std::type_index> types {};
+  if (m_column_types.empty()) {
+    types = autoDetectColumnTypes(in, m_comment, columns_number);
+  } else {
+    types = m_column_types;
+  }
+  auto column_info = createColumnInfo(names, types);
+  
+  std::vector<Row> row_list;
+  regex column_separator {"\\s+"};
+  while (in) {
+    std::string line;
+    getline(in, line);
+    size_t comment_pos = line.find(m_comment);
+    if (comment_pos != std::string::npos) {
+      line = line.substr(0, comment_pos);
+    }
+    boost::trim(line);
+    if (!line.empty()) {
+      boost::sregex_token_iterator i (line.begin(), line.end(), column_separator, -1);
+      boost::sregex_token_iterator j;
+      size_t count {0};
+      std::vector<Row::cell_type> values {};
+      while (i != j) {
+        if (count >= types.size()) {
+          throw ElementsException() << "Line with wrong number of cells: " << line;
+        }
+        values.push_back(convertToCellType(*i, types[count]));
+        ++count;
+        ++i;
+      }
+      row_list.push_back(Row{std::move(values), column_info});
+    }
+  }
+  return Table{std::move(row_list)}; 
+}
+
+}
---
+++ ChTable/src/lib/AsciiReaderHelper.h
@@ -0,0 +1,113 @@
+/** 
+ * @file AsciiReaderHelper.h
+ * @date April 15, 2014
+ * @author Nikolaos Apostolakos
+ */
+
+#ifndef CHTABLE_ASCIIREADERHELPER_H
+#define	CHTABLE_ASCIIREADERHELPER_H
+
+#include <istream>
+#include <string>
+#include <typeindex>
+#include "ChTable/ColumnInfo.h"
+#include "ChTable/Row.h"
+
+namespace ChTable {
+
+/**
+ * @class StreamRewinder
+ * 
+ * @brief
+ * This class gets a stream as argument during construction and when it is deleted
+ * it sets the position of the stream back to where it was during the constructor
+ * call.
+ */
+class StreamRewinder {
+public:
+  StreamRewinder(std::istream& stream) : m_stream(stream), m_position(stream.tellg()) { }
+  ~StreamRewinder() {
+    m_stream.seekg(m_position);
+  }
+private:
+  std::istream& m_stream;
+  int m_position;
+};
+
+/**
+ * @brief
+ * Returns the number of whitespace separated tokens of the first non commented
+ * line
+ * @details
+ * When the method returns the given stream is positioned at the same position
+ * like before the method was called.
+ * 
+ * @param in The string to read from
+ * @param comment The comment pattern
+ * @return The number of columns
+ * @throws ElementsException
+ *    if there is no uncommented, non-empty line
+ */
+size_t countColumns(std::istream& in, const std::string& comment);
+
+/**
+ * @brief
+ * Reads the column names of the given stream
+ * @details
+ * For more information about the auto-detection rules see the constructor of
+ * AsciiReader. When the method returns, the given stream is positioned at the
+ * same position like before the method was called.
+ * 
+ * @param in The stream to read the column names from
+ * @param comment The comment pattern
+ * @param columns_number The number of columns
+ * @return The auto-detected names of the columns
+ * @throws ElementsException
+ *    if there are duplicate column names
+ */
+std::vector<std::string> autoDetectColumnNames(std::istream& in,
+                                               const std::string& comment,
+                                               size_t columns_number);
+
+/**
+ * @brief
+ * Reads the column types of the given stream
+ * @details
+ * For more information about the auto-detection rules see the constructor of
+ * AsciiReader. When the method returns, the given stream is positioned at the
+ * same position like before the method was called.
+ * 
+ * @param in The stream to read the column types from
+ * @param comment The comment pattern
+ * @param columns_number The number of columns
+ * @return The auto-detected types of the columns
+ * @throws ElementsException
+ *    if any of the types is not one of the valid keywords
+ */
+std::vector<std::type_index> autoDetectColumnTypes(std::istream& in,
+                                                   const std::string& comment,
+                                                   size_t columns_number);
+
+/// Creates a ColumnInfo object from the given names and types
+std::shared_ptr<ColumnInfo> createColumnInfo(const std::vector<std::string>& names,
+                                             const std::vector<std::type_index>& types);
+
+/**
+ * @brief
+ * Converts the given value to a Row::cell_type of the given type
+ * @details
+ * For more information of the supported types see the documentation of the
+ * ChTable::AsciiReader constructor.
+ * 
+ * @param value The value to convert
+ * @param type The type of the cell
+ * @return The Row::cell_type representing the value
+ * @throws ElementsException
+ *    if the conversion fails
+ */
+Row::cell_type convertToCellType(const std::string& value, std::type_index type);
+
+}
+
+#endif	/* CHTABLE_ASCIIREADERHELPER_H */
+
