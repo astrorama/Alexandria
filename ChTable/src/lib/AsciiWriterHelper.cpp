065e8a23efd81dcbf11fa269acc73c72ef6f33bf
Nikolaos Apostolakos <nikoapos@gmail.com>
2014-04-16 13:56:41 +0000

    Added the AsciiWriter in the ChTable package.
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Alexandria/branches/Alexandria-2.0@4555 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ ChTable/src/lib/AsciiWriterHelper.cpp
@@ -0,0 +1,51 @@
+/** 
+ * @file AsciiWriterHelper.cpp
+ * @date April 16, 2014
+ * @author Nikolaos Apostolakos
+ */
+
+#include <algorithm>
+#include <boost/lexical_cast.hpp>
+#include "ElementsKernel/ElementsException.h"
+#include "AsciiWriterHelper.h"
+
+namespace ChTable {
+
+std::string typeToKeyword(std::type_index type) {
+  if (type == typeid(bool)) {
+    return "bool";
+  } if (type == typeid(int32_t)) {
+    return "int";
+  } if (type == typeid(int64_t)) {
+    return "long";
+  } if (type == typeid(float)) {
+    return "float";
+  } if (type == typeid(double)) {
+    return "double";
+  } if (type == typeid(std::string)) {
+    return "string";
+  }
+  throw ElementsException() << "Conversion to string for type " << type.name()
+                            << " is not supported";
+}
+
+std::vector<size_t> calculateColumnLengths(const Table& table) {
+  std::vector<size_t> sizes {};
+  // We initialize the values to the required size for the column name and type
+  auto column_info = table.getColumnInfo();
+  for (size_t i=0; i<column_info->size(); ++i) {
+    sizes.push_back(std::max(column_info->getName(i).size(),
+                             typeToKeyword(column_info->getType(i)).size()));
+  }
+  for (auto row : table) {
+    for (size_t i=0; i<sizes.size(); ++i) {
+      sizes[i] = std::max(sizes[i], boost::lexical_cast<std::string>(row[i]).size());
+    }
+  }
+  for (auto& s : sizes) {
+    s += 1;
+  }
+  return sizes;
+}
+
+}
---
+++ ChTable/src/lib/AsciiWriterHelper.h
@@ -0,0 +1,42 @@
+/** 
+ * @file AsciiWriterHelper.h
+ * @date April 16, 2014
+ * @author Nikolaos Apostolakos
+ */
+
+#ifndef CHTABLE_ASCIIWRITERHELPER_H
+#define	CHTABLE_ASCIIWRITERHELPER_H
+
+#include <vector>
+#include <typeindex>
+#include "ChTable/Table.h"
+
+namespace ChTable {
+
+/**
+ * @brief
+ * Converts a type to its string representation
+ * 
+ * @param type The type to convert
+ * @return The string representation
+ * @throws ElementsException
+ *    if the given type is not supported
+ */
+std::string typeToKeyword(std::type_index type);
+
+/**
+ * @brief
+ * Calculates the sizes in characters each column of the table needs
+ * @details
+ * The size is calculated as the size of the longest column entry (including type
+ * and name) plus one to ensure separation of the values. 
+ * 
+ * @param table The table
+ * @return  the sizes of the columns
+ */
+std::vector<size_t> calculateColumnLengths(const Table& table);
+
+}
+
+#endif	/* CHTABLE_ASCIIWRITERHELPER_H */
+
