75e195daf7e2c117870a83f1f53218526c9eb294
Florian Dubath <Florian.Dubath@unige.ch>
2015-11-27 09:50:46 +0000

    Missing file in Alexandria
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Alexandria/trunk@10368 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ MathUtils/src/lib/numericalIntegration/SimpsonsRule.cpp
@@ -0,0 +1,61 @@
+/**
+ * @file MathUtils/src/lib/numericalIntegration/SimpsonsRule.cpp
+ * @date July 2, 2015
+ * @author Florian Dubath
+ */
+
+#include  <cmath>
+#include "MathUtils/numericalIntegration/SimpsonsRule.h"
+#include "ElementsKernel/Exception.h"
+
+namespace Euclid {
+namespace MathUtils {
+
+double SimpsonsRule::operator()(const Function& function, double min,
+    double max, int order) {
+  if (order < 3) {
+    throw Elements::Exception()
+        << "Simpson's Rule integration is define only for order bigger than 2";
+  }
+
+  int N = pow(2, order);
+  double h = (max - min) / N;
+
+  double partial_sum = 0;
+  for (int i = 3; i < N - 2; i++) {
+    partial_sum += function(min + i * h);
+  }
+
+  partial_sum += 0.375 * (function(min) + function(max));
+  partial_sum += 7. * (function(min + h) + function(max - h)) / 6.;
+  partial_sum += 23. * (function(min + 2. * h) + function(max - 2 * h)) / 24.;
+
+  return partial_sum * h;
+
+}
+
+double SimpsonsRule::operator()(const Function& function, double min,
+    double max, double previous_value, int order) {
+  if (order < 4) {
+    throw Elements::Exception()
+        << "Simpson's Rule integration with recursion is define only for order bigger than 3";
+  }
+
+  int N = pow(2, order);
+  double h = (max - min) / N;
+
+  double partial_sum = 0;
+
+  for (int j = 1; j < N / 2 - 1; j++) {
+    int i = 2 * j + 1;
+    partial_sum += function(min + i * h);
+  }
+
+  partial_sum += 7. * (function(min + h) + function(max - h)) / 6.;
+  partial_sum -= 5. * (function(min + 2. * h) + function(max - 2. * h)) / 24.;
+  partial_sum += (function(min + 4. * h) + function(max - 4. * h)) / 24.;
+  return partial_sum * h + previous_value / 2.;
+}
+
+} // End of MathUtils
+} // end of namespace Euclid
