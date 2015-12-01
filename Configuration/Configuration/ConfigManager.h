c04dab031d13b22e7c0aa7b0bd9727c7031cb2af
Nikolaos Apostolakos <nikoapos@gmail.com>
2015-12-01 12:48:56 +0000

    Added tests and documentation for the ConfigManager
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Alexandria/trunk@10443 463500fe-d6be-4184-9b20-47717e75ecac

--- Configuration/Configuration/ConfigManager.h
+++ Configuration/Configuration/ConfigManager.h
@@ -37,13 +37,52 @@ class Configuration;
 
 /**
  * @class ConfigManager
+ * 
  * @brief
- *
+ * Manages a set of configuration classes
+ * 
+ * @details
+ * The ConfigManager is responsible for handling a set of configuration classes.
+ * For flexibility (for the case an executable is called from another one), this
+ * class is not implemented as a singleton, but a factory method is provided,
+ * which returns the manager instances based on an ID.
+ * 
+ * Each manager instance is responsible for managing a set of configurations,
+ * which can be registered by using the registerConfiguration() method. When
+ * this method is used, all the dependencies of the configuration being registered
+ * are also registered automatically.
+ * 
+ * A manager can be under the following states, which describe how it can be used:
+ * - Registration phase:
+ *    This is the state a ConfigManager instance is in right after creation.
+ *    During this phase configuration types can be added by using the
+ *    registerConfiguration() method.
+ * - Waiting for initialization phase:
+ *    A manager goes in this state after a call to the closeRegistration() method.
+ *    From this state on, the registerConfiguration() method cannot be used any
+ *    more. The closeRegistration() returns a boost options_description which
+ *    describes the options for all the managed configurations (it can be called
+ *    more than once).
+ * - Initialization phase:
+ *    The initialization phase starts by calling the initialize() method. This
+ *    method gets as arguments the user parameters to initialize the configurations
+ *    with. During this phase, the manager performs three actions. First, it
+ *    calls the preInitialize() method of all its managed configurations, in
+ *    arbitrary order. Second, it calls the initialize() method of the managed
+ *    configurations, in such order so all dependencies of a configuration have
+ *    already been initialized before its method is called. Finally, it calls
+ *    the postInitialize() methods, again in arbitrary order.
+ * - Initialized phase:
+ *    When all the managed configurations have been initialized (as described
+ *    above) the manager enters the initialized state. At this phase, the user
+ *    can use the getConfiguration() method for retrieving the information he
+ *    needs.
  */
 class ConfigManager {
 
 public:
 
+  /// Returns a reference to the ConfigManager with the given ID
   static ConfigManager& getInstance(long id);
 
   /**
@@ -51,16 +90,98 @@ public:
    */
   virtual ~ConfigManager() = default;
 
+  /**
+   * @brief
+   * Registers a Configuration to the manager
+   * 
+   * @details
+   * After this method is executed all the dependencies of the Configuration will
+   * also be registered (recursively). This method can be executed more than one
+   * time for the same configuration. Consecutive calls have no extra effect.
+   * 
+   * @tparam T
+   *    The type of the Configuration to register
+   * @throws Elements::Exception
+   *    If the manager is closed for registration
+   */
   template <typename T>
   void registerConfiguration();
   
+  /**
+   * @brief
+   * Registers a dependency between two configurations
+   * 
+   * @details
+   * This method can be used to define extra dependencies between configurations
+   * than the ones defined by the configurations themselves. Calling this method
+   * does not register the related configurations. If any of the related
+   * configurations is not registered during the registration phase, the
+   * dependency is ignored.
+   * 
+   * The meaning of the template parameters is that the T1 depends on T2.
+   * 
+   * @tparam T1
+   *    The dependant Configuration type
+   * @tparam T2
+   *    The dependency Configuration type
+   * @throws Elements::Exception
+   *    If the manager is closed for registration
+   */
   template <typename T1, typename T2>
   void registerDependency();
   
+  /**
+   * @brief
+   * Terminates the manager registration phase
+   * 
+   * @details
+   * This call will make a test that there are no circular dependencies between
+   * the registered configurations. The returned options_description can be
+   * used for parsing the user input.
+   * 
+   * @return 
+   *    The options_description describing the parameters of all the managed
+   *    Configurations
+   * @throws Elements::Exception
+   *    If there are circular dependencies between the configurations
+   */
   boost::program_options::options_description closeRegistration();
   
+  
+  /**
+   * @brief
+   * Initialize the manager
+   * 
+   * @details
+   * This method gets as arguments the user parameters to initialize the configurations
+   * with. During the initialization phase, the manager performs three actions. First, it
+   * calls the preInitialize() method of all its managed configurations, in
+   * arbitrary order. Second, it calls the initialize() method of the managed
+   * configurations, in such order so all dependencies of a configuration have
+   * already been initialized before its method is called. Finally, it calls
+   * the postInitialize() methods, again in arbitrary order.
+   * 
+   * When this method returns, all the configurations are in FINAL state and the
+   * manager in INITIALIZED state.
+   * 
+   * @param user_values
+   *    The user values to initialize the configurations with
+   */
   void initialize(const std::map<std::string, boost::program_options::variable_value>& user_values);
   
+  /**
+   * @brief
+   * Returns a reference to the requested configuration
+   * 
+   * @tparam T
+   *    The type of the Configuration
+   * @return
+   *    A reference to the requested configuration
+   * @throws Elements::Exception
+   *    If the manager is not initialized
+   * @throws Elements::Exception
+   *    If the manager does not manage the requested configuration 
+   */
   template <typename T>
   T& getConfiguration();
 
--- Configuration/tests/src/ConfigManager_test.cpp
+++ Configuration/tests/src/ConfigManager_test.cpp
@@ -22,8 +22,102 @@
 #include <boost/test/unit_test.hpp>
 
 #include "Configuration/ConfigManager.h"
+#include "ConfigManager_fixture.h"
 
 using namespace Euclid::Configuration;
+namespace po = boost::program_options;
+
+class Config1 : public Configuration {
+public:
+  Config1(long id) : Configuration(id) {}
+  std::map<std::string, OptionDescriptionList> getProgramOptions() {
+    return {{"Test",{{"par-1", po::value<int>(), ""}}}};
+  }
+  bool pre_initialized = false;
+  bool initialized = false;
+  bool post_initialized = false;
+  void preInitialize(const UserValues&) {
+    BOOST_CHECK(!pre_initialized);
+    BOOST_CHECK(!initialized);
+    BOOST_CHECK(!post_initialized);
+    pre_initialized = true;
+  }
+  void initialize(const UserValues&) {
+    BOOST_CHECK(pre_initialized);
+    BOOST_CHECK(!initialized);
+    BOOST_CHECK(!post_initialized);
+    initialized = true;
+  }
+  void postInitialize(const UserValues&) {
+    BOOST_CHECK(pre_initialized);
+    BOOST_CHECK(initialized);
+    BOOST_CHECK(!post_initialized);
+    post_initialized = true;
+  }
+};
+
+class Config2 : public Configuration {
+public:
+  Config2(long id) : Configuration(id) {}
+  std::map<std::string, OptionDescriptionList> getProgramOptions() {
+    return {{"Test",{{"par-2", po::value<int>(), ""}}}};
+  }
+  bool pre_initialized = false;
+  bool initialized = false;
+  bool post_initialized = false;
+  void preInitialize(const UserValues&) {
+    BOOST_CHECK(!pre_initialized);
+    BOOST_CHECK(!initialized);
+    BOOST_CHECK(!post_initialized);
+    pre_initialized = true;
+  }
+  void initialize(const UserValues&) {
+    BOOST_CHECK(pre_initialized);
+    BOOST_CHECK(!initialized);
+    BOOST_CHECK(!post_initialized);
+    initialized = true;
+  }
+  void postInitialize(const UserValues&) {
+    BOOST_CHECK(pre_initialized);
+    BOOST_CHECK(initialized);
+    BOOST_CHECK(!post_initialized);
+    post_initialized = true;
+  }
+};
+
+class Config3 : public Configuration {
+public:
+  Config3(long id) : Configuration(id) {
+    declareDependency<Config1>();
+    declareDependency<Config2>();
+  }
+  std::map<std::string, OptionDescriptionList> getProgramOptions() {
+    return {{"Test",{{"par-3", po::value<int>(), ""}}}};
+  }
+  bool pre_initialized = false;
+  bool initialized = false;
+  bool post_initialized = false;
+  void preInitialize(const UserValues&) {
+    BOOST_CHECK(!pre_initialized);
+    BOOST_CHECK(!initialized);
+    BOOST_CHECK(!post_initialized);
+    pre_initialized = true;
+  }
+  void initialize(const UserValues&) {
+    BOOST_CHECK(getDependency<Config1>().getCurrentState() == State::INITIALIZED);
+    BOOST_CHECK(getDependency<Config2>().getCurrentState() == State::INITIALIZED);
+    BOOST_CHECK(pre_initialized);
+    BOOST_CHECK(!initialized);
+    BOOST_CHECK(!post_initialized);
+    initialized = true;
+  }
+  void postInitialize(const UserValues&) {
+    BOOST_CHECK(pre_initialized);
+    BOOST_CHECK(initialized);
+    BOOST_CHECK(!post_initialized);
+    post_initialized = true;
+  }
+};
 
 //-----------------------------------------------------------------------------
 
@@ -31,10 +125,125 @@ BOOST_AUTO_TEST_SUITE (ConfigManager_test)
 
 //-----------------------------------------------------------------------------
 
-BOOST_AUTO_TEST_CASE( example_test ) {
+BOOST_FIXTURE_TEST_CASE(registerConfiguration, ConfigManager_fixture) {
 
-  BOOST_FAIL("!!!! Please implement your tests !!!!");
+  // Given
+  config_manager.registerConfiguration<Config3>();
+  
+  // When
+  config_manager.closeRegistration();
+  config_manager.initialize({});
+  
+  // Then
+  BOOST_CHECK_NO_THROW(config_manager.getConfiguration<Config1>());
+  BOOST_CHECK_NO_THROW(config_manager.getConfiguration<Config2>());
+  BOOST_CHECK_NO_THROW(config_manager.getConfiguration<Config3>());
 
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(registerConfigurationWhenClosed, ConfigManager_fixture) {
+  
+  // When
+  config_manager.closeRegistration();
+  
+  // Then
+  BOOST_CHECK_THROW(config_manager.registerConfiguration<Config1>(), Elements::Exception);
+
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(circularDependency, ConfigManager_fixture) {
+  
+  // Given
+  config_manager.registerConfiguration<Config3>();
+  
+  // When
+  config_manager.registerDependency<Config1, Config3>();
+  
+  // Then
+  BOOST_CHECK_THROW(config_manager.closeRegistration(), Elements::Exception);
+
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(getProgramOptions, ConfigManager_fixture) {
+  
+  // Given
+  config_manager.registerConfiguration<Config3>();
+  
+  // When
+  auto options = config_manager.closeRegistration();
+  
+  // Then
+  BOOST_CHECK_NO_THROW(options.find("par-1", false));
+  BOOST_CHECK_NO_THROW(options.find("par-2", false));
+  BOOST_CHECK_NO_THROW(options.find("par-3", false));
+
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(initialize, ConfigManager_fixture) {
+  
+  // Given
+  config_manager.registerConfiguration<Config3>();
+  
+  // When
+  config_manager.closeRegistration();
+  config_manager.initialize({});
+  auto& conf1 = config_manager.getConfiguration<Config1>();
+  auto& conf2 = config_manager.getConfiguration<Config2>();
+  auto& conf3 = config_manager.getConfiguration<Config3>();
+  
+  // Then
+  BOOST_CHECK(conf1.pre_initialized);
+  BOOST_CHECK(conf1.initialized);
+  BOOST_CHECK(conf1.post_initialized);
+  BOOST_CHECK(conf1.getCurrentState() == Configuration::State::FINAL);
+  BOOST_CHECK(conf2.pre_initialized);
+  BOOST_CHECK(conf2.initialized);
+  BOOST_CHECK(conf2.post_initialized);
+  BOOST_CHECK(conf2.getCurrentState() == Configuration::State::FINAL);
+  BOOST_CHECK(conf3.pre_initialized);
+  BOOST_CHECK(conf3.initialized);
+  BOOST_CHECK(conf3.post_initialized);
+  BOOST_CHECK(conf3.getCurrentState() == Configuration::State::FINAL);
+
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(getConfigurationNotInitialized, ConfigManager_fixture) {
+  
+  // Given
+  config_manager.registerConfiguration<Config1>();
+  
+  // When
+  config_manager.closeRegistration();
+  
+  // Then
+  BOOST_CHECK_THROW(config_manager.getConfiguration<Config1>(), Elements::Exception);
+  
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(getConfigurationNotRegistered, ConfigManager_fixture) {
+  
+  // Given
+  config_manager.registerConfiguration<Config1>();
+  
+  // When
+  config_manager.closeRegistration();
+  config_manager.initialize({});
+  
+  // Then
+  BOOST_CHECK_THROW(config_manager.getConfiguration<Config2>(), Elements::Exception);
+  
 }
 
 //-----------------------------------------------------------------------------
