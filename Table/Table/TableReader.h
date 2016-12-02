6abf9f4ebe4ac5788ac32753bf5f75b1430a6a22
Nikolaos Apostolakos <nikoapos@gmail.com>
2016-12-02 07:26:11 +0100

    Add the TableReader interface [Redmine issue #12117]

---
+++ Table/Table/TableReader.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2012-2020 Euclid Science Ground Segment    
+ *  
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free 
+ * Software Foundation; either version 3.0 of the License, or (at your option)  
+ * any later version.  
+ *  
+ * This library is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more  
+ * details.  
+ *  
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA  
+ */
+
+/* 
+ * @file TableReader.h
+ * @author nikoapos
+ */
+
+#ifndef _TABLE_TABLEREADER_H
+#define _TABLE_TABLEREADER_H
+
+#include "Table/Table.h"
+
+namespace Euclid {
+namespace Table {
+
+class TableReader {
+  
+public:
+  
+  TableReader() = default;
+  
+  TableReader(TableReader&&) = default;
+  TableReader& operator=(TableReader&&) = default;
+  
+  TableReader(const TableReader&) = delete;
+  TableReader& operator=(const TableReader&) = delete;
+
+  virtual ~TableReader() = default;
+  
+  virtual ColumnInfo getInfo() = 0;
+
+  virtual Table read(long rows=-1) = 0;
+  
+  virtual void skip(long rows) = 0;
+  
+  virtual bool hasMoreRows() = 0;
+  
+};
+
+} // namespace Table
+} // namespace Euclid
+
+#endif /* _TABLE_TABLEREADER_H */
+
