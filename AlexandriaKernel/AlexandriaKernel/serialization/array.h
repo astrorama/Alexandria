cb810ba0b80d21a5e26abf0af540be1ee9d832c9
Nikolaos Apostolakos <nikoapos@gmail.com>
2018-03-01 12:56:04 +0100

    Update the sd::array serialization to work with versions of boost between 1.56 and 1.63

--- AlexandriaKernel/AlexandriaKernel/serialization/array.h
+++ AlexandriaKernel/AlexandriaKernel/serialization/array.h
@@ -2,6 +2,14 @@
 #ifndef ALEXANDRIA_KERNEL_SERIALIZATION_ARRAY_H
 #define ALEXANDRIA_KERNEL_SERIALIZATION_ARRAY_H
 
+// Boost, starting from version 1.56, provides serialization for the templated
+// std::array. This file provides basic serialization support for versions
+// before that. Note that if the boost version exists it is used instead.
+
+#include <boost/version.hpp>
+
+#if (BOOST_VERSION / 100000) <= 1 && ((BOOST_VERSION / 100) % 1000) < 56
+
 #include <array>
 
 namespace boost {
@@ -16,6 +24,12 @@ void serialize(Archive& archive, std::array<CellType, ND> array, const unsigned 
 
 }
 }
+
+#else
+
+#include <boost/serialization/array.hpp>
+
+#endif
 
 #endif /* ALEXANDRIA_KERNEL_SERIALIZATION_ARRAY_H */
 
