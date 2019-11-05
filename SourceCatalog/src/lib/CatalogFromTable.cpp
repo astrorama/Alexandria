0b055bc138a2ecd4b6b6898a6fec26c50cfe5a9f
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2019-11-05 10:56:53 +0100

    Make CastSourceIdVisitor public
    It can be used by, i.e, Phosphoros, to retrieve both string and numeric
    IDs

--- SourceCatalog/src/lib/CatalogFromTable.cpp
+++ SourceCatalog/src/lib/CatalogFromTable.cpp
@@ -1,21 +1,21 @@
 /*
- * Copyright (C) 2012-2020 Euclid Science Ground Segment    
- *  
+ * Copyright (C) 2012-2020 Euclid Science Ground Segment
+ *
  * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free 
- * Software Foundation; either version 3.0 of the License, or (at your option)  
- * any later version.  
- *  
- * This library is distributed in the hope that it will be useful, but WITHOUT 
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more  
- * details.  
- *  
- * You should have received a copy of the GNU Lesser General Public License 
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA  
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
- 
+
  /**
  * @file src/lib/CatalogFromTable.cpp
  *
@@ -31,31 +31,6 @@
 using namespace std;
 namespace Euclid {
 namespace SourceCatalog {
-
-class CastSourceIdVisitor: public boost::static_visitor<Source::id_type> {
-    template <typename From>
-    static constexpr bool is_integer() {
-      return std::is_integral<From>::value && !std::is_same<From, bool>::value;
-    }
-
-public:
-    CastSourceIdVisitor() {}
-
-    Source::id_type operator() (const std::string &from) const {
-      return from;
-    }
-
-    template <typename From>
-    Source::id_type operator() (const From &from, typename std::enable_if<is_integer<From>()>::type* = 0) const {
-      return Source::id_type(static_cast<int64_t>(from));
-    }
-
-    template <typename From>
-    Source::id_type operator() (const From &, typename std::enable_if<!is_integer<From>()>::type* = 0) const {
-      throw Elements::Exception() << "Only std::string and int64_t are supported types for a source ID, got "
-        << typeid(From).name() << " instead";
-    }
-};
 
 CatalogFromTable::CatalogFromTable(
     std::shared_ptr<Euclid::Table::ColumnInfo> column_info_ptr,
