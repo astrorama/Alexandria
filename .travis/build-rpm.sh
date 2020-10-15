baaed03380369b1ed12c9873faa1f9a3dce5112e
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-10-15 16:19:34 +0200

    Build in Fedora 33 and CentOS 8

--- .travis/build-rpm.sh
+++ .travis/build-rpm.sh
@@ -9,18 +9,26 @@ export BRANCH=$1
 # Platform-specific configuration
 source /etc/os-release
 
-CMAKEFLAGS="-DINSTALL_DOC=ON"
+CMAKEFLAGS="-DINSTALL_DOC=ON -DUSE_SPHINX_APIDOC=OFF"
+
+# Default to python 3
+PYTHON="python3"
 
 if [ "$ID" == "fedora" ]; then
-  if [ "$VERSION_ID" -ge 30 ]; then
-    PYTHON="python3"
-    CMAKEFLAGS="$CMAKEFLAGS -DPYTHON_EXPLICIT_VERSION=3"
-  else
+  if [ "$VERSION_ID" -lt 30 ]; then
     PYTHON="python"
   fi
 elif [ "$ID" == "centos" ]; then
   yum install -y epel-release
-  PYTHON="python"
+  if [ "$VERSION_ID" -ge 8 ]; then
+    sed -i "s/enabled=0/enabled=1/" /etc/yum.repos.d/CentOS-PowerTools.repo
+  else
+    PYTHON="python"
+  fi
+fi
+
+if [ "$PYTHON" == "python3" ]; then
+  CMAKEFLAGS="$CMAKEFLAGS -DPYTHON_EXPLICIT_VERSION=3"
 fi
 
 # Astrorama repository
@@ -54,7 +62,7 @@ yum install -y ${rpm_dev_deps} ${rpm_doc_deps}
 # Dependencies
 yum install -y cmake make gcc-c++ rpm-build
 yum install -y boost-devel $PYTHON-pytest log4cpp-devel doxygen CCfits-devel $PYTHON-numpy
-yum install -y graphviz $PYTHON-sphinx $PYTHON-sphinxcontrib-apidoc
+yum install -y graphviz $PYTHON-sphinx
 
 # Build
 mkdir -p /build
