2f871e8b18a96c739b864628e0a74d711bddfe8b
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2021-05-17 11:13:40 +0200

    Add missing documentation

--- MathUtils/MathUtils/interpolation/GridInterpolation.h
+++ MathUtils/MathUtils/interpolation/GridInterpolation.h
@@ -26,13 +26,47 @@
 namespace Euclid {
 namespace MathUtils {
 
+/**
+ * Interpolate on a grid with arbitrary knot types (discrete or continuous)
+ * @tparam AxisType
+ *  List of axes types. Their order corresponds to the order of the axes on the NdArray with the grid values.
+ *  For instance, values.at(0,1,2) corresponds to the position on the grid defined by the knot 0 of the first
+ *  axis, the knot 1 of the second axis, and the knot 2 of the third axis.
+ *
+ * @details
+ *  Example of usage:
+ *
+ * \code{.cpp}
+ * // First and second axes are continuous, third one is discrete
+ * InterpN<double, double, MyEnum> interp(...);
+ * double interpolated = interp(0.5, 1.48, MyEnum::A)
+ * \endcode
+ *
+ * @warning
+ *  For discrete dimensions, even if extrapolate is true, undefined bins will be considered to have a value of 0.
+ */
 template <typename... AxisType>
 class InterpN {
 public:
+  /**
+   * Constructor
+   * @param grid
+   *    Knots defining the grid
+   * @param values
+   *    Grid values
+   * @param extrapolate
+   *    If true, values will be extrapolated on continuous dimensions
+   */
   InterpN(const std::tuple<std::vector<AxisType>...>& grid, const NdArray::NdArray<double>& values, bool extrapolate);
 
+  /**
+   * Destructor
+   */
   ~InterpN() = default;
 
+  /**
+   * Interpolate the value for the given parameters
+   */
   double operator()(AxisType... args) const;
 };
 
