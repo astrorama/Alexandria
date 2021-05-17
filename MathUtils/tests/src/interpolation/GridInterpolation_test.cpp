34fa1473d981748350d0a6a51b6a05ba493a1eaa
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2021-05-17 14:23:04 +0200

    GridInterpolation: Interpolate with a single knot

--- MathUtils/tests/src/interpolation/GridInterpolation_test.cpp
+++ MathUtils/tests/src/interpolation/GridInterpolation_test.cpp
@@ -68,6 +68,117 @@ BOOST_AUTO_TEST_CASE(CombinedAxesReversed2) {
 
 //-----------------------------------------------------------------------------
 
+BOOST_AUTO_TEST_CASE(CopyConstructor) {
+  std::vector<double>   knots0{0., 1., 2., 3., 4.};
+  std::vector<EnumType> knots1{EnumType::A, EnumType::B, EnumType::C, EnumType::E};
+
+  NdArray<double> values{{5, 4}, {                 //
+                                  1,  4,  3,  5,   //
+                                  6,  9,  10, 11,  //
+                                  90, 70, 60, 65,  //
+                                  1,  1,  1,  1,   //
+                                  0,  3,  0,  0}};
+
+  InterpN<double, EnumType> interp2({knots0, knots1}, values, true);
+
+  BOOST_CHECK_EQUAL(interp2(1., EnumType::A), 6);
+  BOOST_CHECK_CLOSE(interp2(2.5, EnumType::B), 35.5, 1e-8);
+
+  InterpN<double, EnumType> interp_copy(interp2);
+
+  BOOST_CHECK_EQUAL(interp_copy(1., EnumType::A), 6);
+  BOOST_CHECK_CLOSE(interp_copy(2.5, EnumType::B), 35.5, 1e-8);
+  BOOST_CHECK_CLOSE(interp_copy(5., EnumType::C), -1, 1e-8);  // Extrapolated
+  BOOST_CHECK_EQUAL(interp_copy(4., EnumType::D), 0.);        // d is not defined
+  BOOST_CHECK_CLOSE(interp_copy(0.5, EnumType::E), 8., 1e-8);
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_CASE(SingleDiscreteValue) {
+  std::vector<double>   knots0{0., 1., 2., 3.};
+  std::vector<EnumType> knots1{EnumType::A};
+  NdArray<double>       values{{4, 1}, {1., 2., 3., 4.}};
+
+  InterpN<double, EnumType> interp({knots0, knots1}, values, true);
+
+  BOOST_CHECK_CLOSE(interp(1., EnumType::A), 2., 1e-8);
+  BOOST_CHECK_CLOSE(interp(2.5, EnumType::A), 3.5, 1e-8);
+  BOOST_CHECK_EQUAL(interp(1.8, EnumType::B), 0.0);
+
+  // Swap axes
+  InterpN<EnumType, double> interp2({knots1, knots0}, values.reshape(1, 4), true);
+  BOOST_CHECK_CLOSE(interp2(EnumType::A, 1.), 2., 1e-8);
+  BOOST_CHECK_CLOSE(interp2(EnumType::A, 2.5), 3.5, 1e-8);
+  BOOST_CHECK_EQUAL(interp2(EnumType::B, 1.8), 0.0);
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_CASE(SingleContinuousValue) {
+  std::vector<double>   knots0{1.};
+  std::vector<EnumType> knots1{EnumType::A, EnumType::B, EnumType::C};
+  NdArray<double>       values{{1, 3}, {2., 3., 4.}};
+
+  InterpN<double, EnumType> interp({knots0, knots1}, values, true);
+/*
+  BOOST_CHECK_CLOSE(interp(1., EnumType::A), 2., 1e-8);
+  BOOST_CHECK_CLOSE(interp(1., EnumType::B), 3., 1e-8);
+  BOOST_CHECK_CLOSE(interp(1.5, EnumType::B), 3., 1e-8);  // The only sensible extrapolation is assume a constant value
+  BOOST_CHECK_EQUAL(interp(1.8, EnumType::D), 0.0);
+*/
+  // Swap axes
+  InterpN<EnumType, double> interp2({knots1, knots0}, values.reshape(3, 1), true);
+  //BOOST_CHECK_CLOSE(interp2(EnumType::A, 1.), 2., 1e-8);
+  BOOST_CHECK_CLOSE(interp2(EnumType::A, 2.5), 2., 1e-8);
+  //BOOST_CHECK_CLOSE(interp2(EnumType::B, 1.), 3., 1e-8);
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_CASE(BadConstructor) {
+  std::vector<double>   knots0{0., 1., 2., 3., 4.};
+  std::vector<EnumType> knots1{EnumType::A, EnumType::B, EnumType::C, EnumType::E};
+
+  NdArray<double> values{{2, 2}};
+
+  // Bad grid shape
+  try {
+    InterpN<double, EnumType> x({knots0, knots1}, values, true);
+    BOOST_FAIL("Constructor should have thrown");
+  } catch (const InterpolationException&) {
+  }
+
+  // Knots not sorted
+  try {
+    InterpN<double, double> x({std::vector<double>{2, 0}, std::vector<double>{1, 2}}, values, true);
+    BOOST_FAIL("Constructor should have thrown");
+  } catch (const InterpolationException&) {
+  }
+
+  // Grid does not have proper number of axes
+  try {
+    InterpN<double, double, double> x{{std::vector<double>{1, 2}, std::vector<double>{3, 4}, std::vector<double>{1}}, values, true};
+    BOOST_FAIL("Constructor should have thrown");
+  } catch (const InterpolationException&) {
+  }
+
+  try {
+    InterpN<double> x{{std::vector<double>{1, 2, 3, 4}}, values, true};
+    BOOST_FAIL("Constructor should have thrown");
+  } catch (const InterpolationException&) {
+  }
+
+  // Good dimensions, bad size
+  try {
+    InterpN<double, double> x{{std::vector<double>{1, 2, 3}, std::vector<double>{5, 6}}, values, true};
+    BOOST_FAIL("Constructor should have thrown");
+  } catch (const InterpolationException&) {
+  }
+}
+
+//-----------------------------------------------------------------------------
+
 BOOST_AUTO_TEST_SUITE_END()
 
 //-----------------------------------------------------------------------------
