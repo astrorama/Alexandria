633c1b20564b05c73b8471324a13d1cea9e03482
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2022-08-30 13:07:46 +0200

    Compute std::log(init_sigma) once

--- SOM/src/lib/NeihbourhoodFunc.cpp
+++ SOM/src/lib/NeihbourhoodFunc.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2021 Euclid Science Ground Segment
+ * Copyright (C) 2012-2022 Euclid Science Ground Segment
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -17,8 +17,8 @@
  */
 
 #include "SOM/NeighborhoodFunc.h"
+#include <algorithm>  // std::max
 #include <cmath>
-#include <algorithm> // std::max
 
 namespace Euclid {
 namespace SOM {
@@ -27,11 +27,12 @@ namespace NeighborhoodFunc {
 Signature linearUnitDisk(double initial_radius) {
   double r_square = initial_radius * initial_radius;
   return [r_square](std::pair<std::size_t, std::size_t> bmu, std::pair<std::size_t, std::size_t> cell,
-                    std::size_t iteration, std::size_t total_iterations) -> double {
-    double iter_factor = 1.0 * (total_iterations - iteration) / total_iterations;
+                    std::size_t iteration, std::size_t total_iterations) {
+    double iter_factor =
+        1.0 * static_cast<double>(total_iterations - iteration) / static_cast<double>(total_iterations);
     iter_factor        = iter_factor * iter_factor;  // We compare the squared distances
-    double x           = bmu.first - cell.first;
-    double y           = bmu.second - cell.second;
+    auto   x           = static_cast<double>(bmu.first - cell.first);
+    auto   y           = static_cast<double>(bmu.second - cell.second);
     double dist_square = x * x + y * y;
     if (dist_square < r_square * iter_factor) {
       return 1.;
@@ -43,25 +44,26 @@ Signature linearUnitDisk(double initial_radius) {
 
 Signature kohonen(std::size_t x_size, std::size_t y_size, double sigma_cutoff_mult) {
 
-  double                                       init_sigma = std::max(x_size, y_size) / 2.;
+  double                                       init_sigma = static_cast<double>(std::max(x_size, y_size)) / 2.;
   std::tuple<std::size_t, std::size_t, double> sigma_buffer{0, 0, 0.};
   double                                       cutoff_mult_square = sigma_cutoff_mult * sigma_cutoff_mult;
+  double                                       sigma_log          = std::log(init_sigma);
 
-  return [init_sigma, sigma_buffer, cutoff_mult_square](std::pair<std::size_t, std::size_t> bmu,
-                                                        std::pair<std::size_t, std::size_t> cell, std::size_t iteration,
-                                                        std::size_t total_iterations) mutable -> double {
+  return [init_sigma, sigma_log, sigma_buffer,
+          cutoff_mult_square](std::pair<std::size_t, std::size_t> bmu, std::pair<std::size_t, std::size_t> cell,
+                              std::size_t iteration, std::size_t total_iterations) mutable {
     // If we have new iteration we recompute the sigma, otherwise we use the already
     // calculated one
     if (std::get<0>(sigma_buffer) != iteration || std::get<1>(sigma_buffer) != total_iterations) {
       std::get<0>(sigma_buffer) = iteration;
       std::get<1>(sigma_buffer) = total_iterations;
-      double time_constant      = total_iterations / std::log(init_sigma);
-      std::get<2>(sigma_buffer) = init_sigma * std::exp(-1. * iteration / time_constant);
+      double time_constant      = static_cast<double>(total_iterations) / sigma_log;
+      std::get<2>(sigma_buffer) = init_sigma * std::exp(-1. * static_cast<double>(iteration) / time_constant);
     }
     double sigma_square = std::get<2>(sigma_buffer) * std::get<2>(sigma_buffer);
 
-    double x           = static_cast<double>(bmu.first) - cell.first;
-    double y           = static_cast<double>(bmu.second) - cell.second;
+    auto   x           = static_cast<double>(bmu.first - cell.first);
+    auto   y           = static_cast<double>(bmu.second - cell.second);
     double dist_square = x * x + y * y;
 
     if (dist_square < cutoff_mult_square * sigma_square) {
