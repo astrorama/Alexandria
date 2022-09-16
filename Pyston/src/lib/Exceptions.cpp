45c6cece1fce7ec54dce83cdd5441cedbcf02aa7
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2022-09-16 13:59:01 +0200

    Fix (C) update

--- Pyston/src/lib/Exceptions.cpp
+++ Pyston/src/lib/Exceptions.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Euclid Science Ground Segment
+ * Copyright (C) 2012-2022 Euclid Science Ground Segment
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
