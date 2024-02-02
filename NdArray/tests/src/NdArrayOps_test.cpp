7656e73d3e9c63d3ea6e1e4145ed2ff499df7b3b
DubathF <florian.dubath@unige.ch>
2024-02-02 15:20:47 +0100

    Add elements wise addition and multiplication to the NdArray<T>

--- NdArray/tests/src/NdArrayOps_test.cpp
+++ NdArray/tests/src/NdArrayOps_test.cpp
@@ -30,6 +30,24 @@ struct OpsFixture {
                               42, 32, 41, 47, 11, 33, 59, 58, 54, 2, 45, 4,  17, 23, 7,  49, 57, 40, 19, 18,
                               6,  39, 24, 36, 50, 52, 12, 38, 37, 8, 15, 30, 5,  20, 26, 43, 28, 10, 27, 44,
                               35, 14, 25, 1,  31, 21, 29, 48, 9,  0, 51, 34, 16, 55, 53, 46, 13, 3,  22, 56}};
+
+  NdArray<int>    other_one_axis{{3}, {4, 5, 6}};
+  NdArray<float>  other_one_axis_float{{3}, {4, 5, 6}};
+  NdArray<float>  other_two_axes{{3, 4}, {17, 18, 13, 14, 15, 12, 11, 112, 111, 16, 110, 19}};
+  NdArray<double> other_three_axes{{3, 4, 5},
+							       {//
+							        10, 11, 12, 13, 14, 15, 16, 17, 18,  1,  2,  3, 4,   5,  6,  7,  8,  9, 10, 11,
+							        21, 29, 24, 26, 20, 22, 22, 28, 27, 28, 25, 20, 2,  20, 26, 23, 28, 20, 27, 24,
+							        35, 14, 25, 1,  31, 21, 29, 48, 9,  0, 51, 34, 16, 55, 53, 46, 13, 3,  22, 56}};
+
+  NdArray<int>    smaller_one_axis{{2}, {4, 5}};
+  NdArray<float>  smaller_one_axis_float{{2}, {4, 5}};
+  NdArray<float>  smaller_two_axes{{3, 3}, {17, 18, 13, 14, 15, 12, 11, 112, 111}};
+  NdArray<double> smaller_three_axes{{3, 4, 4},
+ 	 	                             {//
+ 	 	                              10, 11, 12, 13, 14, 15, 16, 17, 18,  1,  2,  3, 4,   5,  6,  7,
+ 	 	                              21, 29, 24, 26, 20, 22, 22, 28, 27, 28, 25, 20, 2,  20, 26, 23,
+ 	 	                              35, 14, 25, 1,  31, 21, 29, 48, 9,  0, 51, 34, 16, 55, 53, 46}};
 };
 
 //-----------------------------------------------------------------------------
@@ -272,6 +290,69 @@ BOOST_FIXTURE_TEST_CASE(Sort_test, OpsFixture) {
   };
   BOOST_CHECK_EQUAL_COLLECTIONS(named.begin(), named.end(), expected.begin(), expected.end());
 }
+
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(SameShape_test, OpsFixture) {
+	 // Check with self and other with same shape
+	 BOOST_CHECK(sameShape(one_axis, one_axis));
+	 BOOST_CHECK(sameShape(one_axis, other_one_axis));
+	 BOOST_CHECK(sameShape(one_axis_float, one_axis_float));
+	 BOOST_CHECK(sameShape(one_axis_float, other_one_axis_float));
+	 BOOST_CHECK(sameShape(one_axis, one_axis_float));
+	 BOOST_CHECK(sameShape(two_axes, two_axes));
+	 BOOST_CHECK(sameShape(two_axes, other_two_axes));
+	 BOOST_CHECK(sameShape(three_axes, three_axes));
+	 BOOST_CHECK(sameShape(three_axes, other_three_axes));
+
+	 // Check with other dim
+	 BOOST_CHECK(!sameShape(one_axis, two_axes));
+	 BOOST_CHECK(!sameShape(one_axis_float, two_axes));
+	 BOOST_CHECK(!sameShape(one_axis, three_axes));
+	 BOOST_CHECK(!sameShape(one_axis_float, three_axes));
+	 BOOST_CHECK(!sameShape(two_axes, three_axes));
+
+	 // Check with same dim but different shapes.
+	 BOOST_CHECK(!sameShape(one_axis, smaller_one_axis));
+	 BOOST_CHECK(!sameShape(one_axis_float, smaller_one_axis_float));
+	 BOOST_CHECK(!sameShape(two_axes, smaller_two_axes));
+	 BOOST_CHECK(!sameShape(three_axes, smaller_three_axes));
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(multiplyElements_test, OpsFixture) {
+	 // Check throw on different shape
+	 BOOST_CHECK_THROW(multiplyElements(one_axis_float, two_axes), std::invalid_argument);
+	 BOOST_CHECK_THROW(multiplyElements(one_axis, smaller_one_axis), std::invalid_argument);
+
+	 // Nominal use
+	 auto one_d = multiplyElements(one_axis, other_one_axis);
+	 std::vector<int> expected_one_d{4, 10, 18 };
+	 BOOST_CHECK_EQUAL_COLLECTIONS(one_d.begin(), one_d.end(), expected_one_d.begin(), expected_one_d.end());
+
+	 auto two_d = multiplyElements(two_axes, other_two_axes);
+	 std::vector<int> expected_two_d{119, 144, 39, 56, 75, 24, 11, 1344, 1221, 96, 1100, 171};
+	 BOOST_CHECK_EQUAL_COLLECTIONS(two_d.begin(), two_d.end(), expected_two_d.begin(), expected_two_d.end());
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE(addElements_test, OpsFixture) {
+	 // Check throw on different shape
+	 BOOST_CHECK_THROW(addElements(one_axis_float, two_axes), std::invalid_argument);
+	 BOOST_CHECK_THROW(addElements(one_axis, smaller_one_axis), std::invalid_argument);
+
+	 // Nominal use
+	 auto one_d = addElements(one_axis, other_one_axis);
+	 std::vector<int> expected_one_d{5, 7, 9};
+	 BOOST_CHECK_EQUAL_COLLECTIONS(one_d.begin(), one_d.end(), expected_one_d.begin(), expected_one_d.end());
+
+	 auto two_d = addElements(two_axes, other_two_axes);
+	 std::vector<int> expected_two_d{24, 26, 16, 18, 20, 14, 12, 124, 122, 22, 120, 28};
+	 BOOST_CHECK_EQUAL_COLLECTIONS(two_d.begin(), two_d.end(), expected_two_d.begin(), expected_two_d.end());
+}
+
 
 //-----------------------------------------------------------------------------
 
